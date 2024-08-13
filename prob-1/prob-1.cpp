c9a9dc17454d122710c1d28fa49f5896ad78b4ba
ateeq <atiqueraza98@gmail.com>
2024-08-14 00:37:46 +0530

    Fix comment in prob-1

--- prob-1/prob-1.cpp
+++ prob-1/prob-1.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-/*Problem Statement : Find the sum of all the multiples of 3 or 5 below 1000.*/
+/*Problem 1: Find the sum of all the multiples of 3 or 5 below 1000.*/
 
 
 int main()
