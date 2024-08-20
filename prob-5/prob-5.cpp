92355ea46ae7a911f227daf0c1aec7c4a0fb4969
ateeq <atiqueraza98@gmail.com>
2024-08-20 13:41:13 +0530

    Finish Problem

--- prob-5/prob-5.cpp
+++ prob-5/prob-5.cpp
@@ -1,2 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+
+ll gcd(ll a, ll b){
+
+    ll t;
+
+    while(b != 0){
+        t = b;
+        b = a % b;
+        a = t;
+    }
+
+    return a;
+}
+
+ll calc_lcm(ll a, ll b){
+    return (a*b) / gcd(a,b);
+}
+
+
+
+
+int main(){
+
+    ll lcm = 1;
+
+    for (int i = 2; i <= 20; i++)
+    {
+        lcm = calc_lcm(lcm, i);
+    }
+    
+
+
+
+    cout << lcm;
+
+
+}
