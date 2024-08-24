085cf20500d38a2f2e504626d76fdeb9caaa07a0
ateeq <atiqueraza98@gmail.com>
2024-08-24 19:20:30 +0530

    finish Problem

--- prob-6/prob-6.cpp
+++ prob-6/prob-6.cpp
@@ -1,2 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int main(){
+
+    int sumOfSquares = 0;
+    for(int i = 1; i<=100; i++){
+        int currentSquare = i * i;
+        sumOfSquares += currentSquare;
+    }
+
+    int sum = 0;
+    for(int i = 1; i <=100; i++)
+    {
+        sum = sum + i;
+    }
+
+
+    int squareOfSum = sum * sum;
+
+    int diff = squareOfSum - sumOfSquares;
+
+    cout << diff;
+
+
+}
